6b68d019484e416f3ef4a15d1979f34d88273de7
cdelauder <cdelauder@gmail.com>
2014-08-24 19:31:16 -0700

    added the vr module. like the previous ones it includes a get and set method.

--- js/request.js
+++ js/request.js
@@ -48,10 +48,40 @@ var CreditCard = (function() {
 
 }())
 
+var VerificationRequest = (function() {
+  var vr = {
+    set: function(avsRequest, cvv2Request) {
+      this.attribute = {}
+      this.attribute.avsRequest = avsRequest
+      this.attribute.cvv2Request = cvv2Request
+    },
+    get: function() {
+      return vr.attribute
+    }
+  }
+
+  return {
+    new : function(avsRequest, cvv2Request) {
+      vr.set(avsRequest, cvv2Request)
+      return vr.get()
+    }
+
+  }
+}())
+
+$AVS_VERIFY_STREET_AND_ZIP = 0
+$AVS_VERIFY_ZIP_ONLY = 1
+
+$CVV2_NOT_SUBMITTED = 0
+$CVV2_PRESENT = 1
+$CVV2_PRESENT_BUT_ILLEGIBLE = 2
+$CVV2_HAS_NO_CVV2 = 9
 var merchantId = 1111111
 var apiToken = '1e1e1e1e1e1e1e1e1e1ee'
 var url = "https://test.salt.com/gateway/creditcard/processor.do"
 var service = HttpsCreditCardService.new(merchantId, apiToken, url)
 var card = CreditCard.new("4242424242424242", "1010", "111", "123 Street", "A1B2C3")
+var vr = VerificationRequest.new($AVS_VERIFY_STREET_AND_ZIP, $CVV2_PRESENT)
 console.log(service)
 console.log(card)
+console.log(vr)
