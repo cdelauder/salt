ce56bd55074573c8179a283446ccb076cb528eb1
cdelauder <cdelauder@gmail.com>
2014-08-24 19:20:11 -0700

    added the credit card module. instantiates a new card and returns the credit card object

--- js/request.js
+++ js/request.js
@@ -22,8 +22,36 @@ var HttpsCreditCardService = (function() {
   }
 } ())
 
+var CreditCard = (function() {
+
+  var card = {
+    set: function(cardNumber, expiryDate, cvv2, street, zip) {
+      this.attribute = {}
+      this.attribute.cardNumber = cardNumber
+      this.attribute.expiryDate = expiryDate
+      this.attribute.cvv2 = cvv2
+      this.attribute.street = street
+      this.attribute.zip = zip
+      this.attribute.secureCode = null
+    },
+    get: function() {
+      return this.attribute
+    }
+  }
+
+  return {
+    new : function(cardNumber, expiryDate, cvv2, street, zip) {
+      card.set(cardNumber, expiryDate, cvv2, street, zip)
+      return card.get()
+    }
+  }
+
+}())
+
 var merchantId = 1111111
 var apiToken = '1e1e1e1e1e1e1e1e1e1ee'
 var url = "https://test.salt.com/gateway/creditcard/processor.do"
 var service = HttpsCreditCardService.new(merchantId, apiToken, url)
+var card = CreditCard.new("4242424242424242", "1010", "111", "123 Street", "A1B2C3")
 console.log(service)
+console.log(card)
